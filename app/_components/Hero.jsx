0ed38765f4090621d3c9ae15cb65db23e28e87ed
LavanyaPathak17 <114918785+LavanyaPathak17@users.noreply.github.com>
2025-03-09 12:08:52 +0530

    added carousel

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,32 +1,20 @@
 import React from "react";
-import Image from "next/image";
-import { ContainerScroll } from "../../components/ui/container-scroll-animation";
+import Carousel from "@/components/ui/Carousel";
 
 function Hero() {
   return (
-    <section className="bg-gray-50 flex items-center flex-col">
-      <div className="flex flex-col overflow-hidden">
-        <ContainerScroll
-          titleComponent={
-            <>
-              <h1 className="text-4xl font-semibold text-black dark:text-white">
-                Manage your Money with AI-Driven Personal <br />
-                <span className="text-4xl md:text-[6rem] text-blue-800 font-bold mt-1 leading-none">
-                  Finance Advisor
-                </span>
-              </h1>
-            </>
-          }
-        >
-          <Image
-            src={`/dashboard.png`}
-            alt="hero"
-            height={720}
-            width={1400}
-            className="mx-auto rounded-2xl object-cover h-full object-left-top"
-            draggable={false}
-          />
-        </ContainerScroll>
+    <section className="bg-gray-50 flex flex-col items-center text-center p-10">
+      <h1 className="text-4xl font-semibold text-black">
+        Finance made simple for women by women <br />
+        <span className="text-4xl md:text-[6rem] text-blue-800 font-bold mt-1 leading-none">
+          Finance Advisor
+        </span>
+      </h1>
+
+   
+  
+      <div className="mt-6 w-full max-w-3xl">
+        <Carousel />
       </div>
     </section>
   );
