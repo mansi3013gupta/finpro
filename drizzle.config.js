41a0915118e2cb5fd8a19c09420a28b42794e49e
mansi3013gupta <mansigupta3013@gmail.com>
2025-03-09 09:56:05 +0530

    addfile

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,8 +3,8 @@ export default {
   schema: "./utils/schema.jsx",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgresql://finan-smart_owner:uk3aed9QZotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/finan-smart?sslmode=require",
+    url: "postgresql://neondb_owner:npg_0zBrPQc2TSsA@ep-muddy-bread-a8cbwov1-pooler.eastus2.azure.neon.tech/fin-smart?sslmode=require",
     connectionString:
-      "postgresql://finan-smart_owner:uk3aed9QZotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/finan-smart?sslmode=require",
+      "postgresql://neondb_owner:npg_0zBrPQc2TSsA@ep-muddy-bread-a8cbwov1-pooler.eastus2.azure.neon.tech/fin-smart?sslmode=require",
   },
 };
